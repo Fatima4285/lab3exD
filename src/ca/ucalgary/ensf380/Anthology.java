d64e3f8a52a0b61b625c220bec592ccceb94d536
Aroush Qureshi <aroushq1@gmail.com>
2024-07-17 18:31:59 -0600

    anthology.java

--- src/ca/ucalgary/ensf380/Anthology.java
+++ src/ca/ucalgary/ensf380/Anthology.java
@@ -1,5 +1,17 @@
 package ca.ucalgary.ensf380;
 
 class Anthology {
-
+	private Story[] stories;
+	
+	public String storyOrder() {
+		return "Method storyOrder called from Anthology";
+	}
+	
+	public Story[] getStory() {
+		return stories;
+	}
+	
+	public void setStory(Story[] stories) {
+		this.stories = stories;
+	}
 }
